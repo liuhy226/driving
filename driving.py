c87afcecba3d63100edc169462d8c4aa3782f13a
liuhongyuan <liuhy226@gmail.com>
2019-05-23 17:52:06 +0800

    update us

--- driving.py
+++ driving.py
@@ -5,3 +5,10 @@ if country == "中国":
         print("你可以考驾照")
     else:
         print("你不可以考驾照")
+elif country == "美国":
+    if age >= 16:rib
+        print("你可以考驾照")
+    else:
+        print("你不可以考驾照")
+else:
+    print("必须输入美国或者中国")
